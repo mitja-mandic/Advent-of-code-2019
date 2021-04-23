00e23cfa8351a81f5d07844b5385c4e79be5675f
Mitja Mandić <49949550+mitja-mandic@users.noreply.github.com>
2021-04-23 11:22:22 +0200

    Dan 1

--- aoc-2019/src/day1/Day1.java
+++ aoc-2019/src/day1/Day1.java
@@ -9,7 +9,8 @@ import java.io.PrintWriter;
 public class Day1 {
 
 	public static void main(String[] args) throws IOException{
-		System.out.println(gorivo("input/day1.txt", "output/day1_out.txt"));
+		System.out.println(gorivo("input/day1.txt", "output/day1_out1.txt"));
+		System.out.println(gorivoZaGorivo("input/day1.txt", "output/day1_out2.txt"));
 
 	}
 	//Prvi del
@@ -28,5 +29,24 @@ public class Day1 {
 	}
 	
 	//Drugi del
+	public static int gorivoZaGorivo(String inputFile, String outputFile) throws IOException {
+		BufferedReader vhod = new BufferedReader(new FileReader(inputFile));
+        PrintWriter izhod = new PrintWriter(new FileWriter(outputFile));
+        int gorivo = 0;
+        while (vhod.ready()) {
+        	
+        	int masa = Integer.parseInt(vhod.readLine().trim());
+        	
+        	while (masa > 6) {
+        		int novaMasa = (int) Math.floor(masa / 3) - 2;
+        		gorivo += novaMasa;
+        		masa = novaMasa;
+        	}
+        }
+        izhod.println(gorivo);
+        vhod.close();
+        izhod.close();
+        return gorivo;
+	}
 	
 }
