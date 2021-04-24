3bd11bd850d9ec311ab149d70fea766b4212e4b1
Mitja Mandić <49949550+mitja-mandic@users.noreply.github.com>
2021-04-24 19:01:13 +0200

    Dan 4 drugi del špaget

--- aoc-2019/src/day4/Day4.java
+++ aoc-2019/src/day4/Day4.java
@@ -4,7 +4,7 @@ public class Day4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(steviloGesel(172930,683082));
+		System.out.println(druga(172930,683082));
 
 	}
 	public static int steviloGesel(int spodnja, int zgornja) {
@@ -17,6 +17,7 @@ public class Day4 {
 			while(x > 0) {
 				int trenutni = x % 10;
 				if(trenutni == zadnji) enaki += 1;
+				
 				else if (zadnji<trenutni) {
 					narasca = false;
 					break;
@@ -25,6 +26,38 @@ public class Day4 {
 				x /= 10;
 			}
 			if(enaki>=1 && narasca) stevilo +=1;
+		}
+		return stevilo;
+	}
+	public static int druga(int spodnja, int zgornja) {
+		int stevilo = 0;
+		for(int geslo = spodnja; geslo<=zgornja; geslo++) {
+			int zadnji = 10; 
+			int enaki = 0;
+			boolean narasca = true;
+			boolean dvojne = false;
+			int ponovitve = 1;
+			int x = geslo;
+			while(x > 0) {
+				int trenutni = x % 10;
+				if(trenutni == zadnji) {
+					enaki += 1;
+					ponovitve += 1;
+				}
+				else {
+					if (zadnji<trenutni) {
+						narasca = false;
+						break;
+					}
+					if(ponovitve == 2) dvojne = true;
+					ponovitve = 1;
+				}
+				zadnji = trenutni;
+				x /= 10;
+			}
+			if(ponovitve == 2) dvojne = true;
+			
+			if(enaki>=1 && narasca && dvojne) stevilo +=1;
 		}
 		return stevilo;
 	}
